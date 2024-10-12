b647dede3a4702de3fdbcee789f3315524cc7209
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-10-12 20:35:20 +0300

    Completed Loops Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name = ['Hamza', 'Ali', 'Musa'], event = 'amazing'){
+    const messages =[];
+    for(let x = 0; x < name.length; x++){
+        messages.push(`Thank you, ${name[x]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(count){
+    while(count >= 0){
+        console.log(count);
+        count--;
+    }
+    return count;
+}
+countDown(10);
